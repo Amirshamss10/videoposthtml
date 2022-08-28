7dcbf12f815d39083cb1722444a2a87aac92ad22
tinar <shamsamir3333@gmail@gmail.com>
2022-08-28 14:46:03 +0430

    add Breeze Authentication

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,24 @@
 <?php
-    use Illuminate\Support\Facades\Route;
-    use Illuminate\Support\Facades\Auth;
-    use App\Http\Controllers\IndexController; 
-    use App\Http\Controllers\VideoController;
-    use App\Http\Controllers\CategoryVideoController;
-    use App\Models\User;
-    /*
-    |--------------------------------------------------------------------------
-    | Web Routes
-    |--------------------------------------------------------------------------
-    |
-    | Here is where you can register web routes for your application. These
-    | routes are loaded by the RouteServiceProvider within a group which
-    | contains the "web" middleware group. Now create something great!
-    |
-    */
 
-    Route::get("/", [IndexController::class, "index"])->name("index");
- 
-    Route::get("/videos/create", [VideoController::class, "create"])->name("videos.create"); 
-    Route::post("/videos", [VideoController::class, "store"])->name("videos.store");
-    Route::get("/videos/{video}", [VideoController::class, "show"])->name("videos.show"); 
-    Route::get("/videos/{video}/edit", [VideoController::class, "edit"])->name("videos.edit");
-    Route::post("/videos/{video}", [VideoController::class,"update"])->name("videos.update");
+use Illuminate\Support\Facades\Route;
 
-    Route::get("/categories/{category}/videos", [CategoryVideoController::class, "index"])->name("categories.videos.index"); 
-?>
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
